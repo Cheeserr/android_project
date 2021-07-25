c99d911cdea66fd03e0fa9c66b640741279de95c
ssp14 <ssp14@aber.ac.uk>
2021-07-25 14:06:14 +0100

    More changes

--- app/src/main/java/com/example/quizz_app/TeacherActivity.java
+++ app/src/main/java/com/example/quizz_app/TeacherActivity.java
@@ -2,22 +2,15 @@ package com.example.quizz_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.PopupWindow;
+import android.widget.Switch;
 import android.widget.Toast;
-
-import java.util.ArrayList;
 
 public class TeacherActivity extends AppCompatActivity {
 
@@ -28,12 +21,29 @@ public class TeacherActivity extends AppCompatActivity {
         setContentView(R.layout.activity_teacher_view);
 
         Button teacherButton = findViewById(R.id.createBankButton);
-        //teacherButton.setOnClickListener(v -> createBank());
+        teacherButton.setOnClickListener(v -> createBank(null));
+
+        Switch simpleSwitch = findViewById(R.id.viewSwitch);
+
+
     }
 
 
     // FR1
-    void createBank(){
+    void createBank(String name){
+        SQLiteOpenHelper databaseHelper = new DatabaseHelper(this);
+        try {
+            SQLiteDatabase db = databaseHelper.getWritableDatabase();
+            ContentValues bankValues = new ContentValues();
+
+            bankValues.put("Name", name);
+            bankValues.put("NUMBEROFQUESTIONS", 0);
+            db.insert("QUESTIONBANKS", null, bankValues);
+            db.close();
+        } catch(SQLiteException e){
+            Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 
     // FR2
@@ -42,6 +52,22 @@ public class TeacherActivity extends AppCompatActivity {
     }
     // FR3
     void listQuestions(){
+
+        SQLiteOpenHelper databaseHelper = new DatabaseHelper(this);
+        try {
+            SQLiteDatabase db = databaseHelper.getReadableDatabase();
+            Cursor cursor = db.query("QUESTIONS", new String[] {"_id","BANKID","QUESTION"},
+                    null, null, null, null, null);
+            if(cursor.moveToFirst()){
+                int bankID = cursor.getInt(0);
+                String question = cursor.getString(1);
+            }
+            cursor.close();
+            db.close();
+        } catch(SQLiteException e){
+            Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
 
     }
     // FR3
@@ -53,18 +79,20 @@ public class TeacherActivity extends AppCompatActivity {
 
         SQLiteOpenHelper databaseHelper = new DatabaseHelper(this);
         try {
-        SQLiteDatabase db = databaseHelper.getWritableDatabase();
+        SQLiteDatabase db = databaseHelper.getReadableDatabase();
         Cursor cursor = db.query("QUESTIONBANKS", new String[] {"_id","NAME","NUMBEROFQUESTIONS"},
                 null, null, null, null, null);
         if(cursor.moveToFirst()){
             String nameBank = cursor.getString(0);
             int numberOfQuestions = cursor.getInt(1);
         }
-
+        cursor.close();
+        db.close();
          } catch(SQLiteException e){
          Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
          toast.show();
          }
+
     }
     // FR5
     void deleteBank(){
